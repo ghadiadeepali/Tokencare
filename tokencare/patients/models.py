1e1e3ceb3f219afd79a8d15ab3cef296703ed65a
ghadiadeepali <deepalinghadia1606@gmail.com>
2025-06-28 18:14:45 +0530

    created Patient model

--- tokencare/patients/models.py
+++ tokencare/patients/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Patient(models.Model):
+    name = models.CharField(max_length=100, null=False)
+    phone_no = models.CharField(max_length=10, blank=True, unique=True)
+    age = models.IntegerField(null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"{self.name} ({self.id})"
