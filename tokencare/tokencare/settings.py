60b085ca3ef08cbbb0a12bf8bd9e033405088e77
ghadiadeepali <deepalinghadia1606@gmail.com>
2025-06-28 18:16:39 +0530

    configured mysql db connection and added patients and rest_framework to INSTALLED_APPS

--- tokencare/tokencare/settings.py
+++ tokencare/tokencare/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "patients",
+    "rest_framework"
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'tokencare.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tokencare',
+        'USER' : 'root',
+        'PASSWORD' : 'admin',
+        'HOST' : '127.0.0.1',
+        'PORT' : '3306',
     }
 }
 
