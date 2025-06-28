9c464062ab1aadbc799a865047dc115def8b71ad
ghadiadeepali <deepalinghadia1606@gmail.com>
2025-06-28 18:19:58 +0530

    changed add_patient view to add_patient_public_api

--- tokencare/patients/urls.py
+++ tokencare/patients/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from patients.views import get_patients, add_patient
+from patients.views import get_patients, add_patient_public_api
 
 urlpatterns = [
     path('',get_patients),
-    path("add/",add_patient)
+    path("add/",add_patient_public_api)
 ]
