1bd7527b138648f898d2eb0d273f44e3cd615ca7
ghadiadeepali <deepalinghadia1606@gmail.com>
2025-07-01 18:22:44 +0530

    added tokens app to INSTALLED_APPS

--- tokencare/tokencare/settings.py
+++ tokencare/tokencare/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "patients",
-    "rest_framework"
+    "rest_framework",
+    "tokens"
 ]
 
 MIDDLEWARE = [
