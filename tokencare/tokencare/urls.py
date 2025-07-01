13dbb3b72e6b52b031ee5a036da79d431d711315
ghadiadeepali <deepalinghadia1606@gmail.com>
2025-07-01 18:14:54 +0530

    modified url pattern to start with /api/v1/

--- tokencare/tokencare/urls.py
+++ tokencare/tokencare/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('patients/', include('patients.urls'))
+    path('api/v1/patients/', include('patients.urls'))
 ]
