0ff53f5b08fac8aa6b86aacdb11f2196223baf16
ghadiadeepali <deepalinghadia1606@gmail.com>
2025-07-01 22:00:53 +0530

    created generate_token function to assign patients token number

--- tokencare/tokens/views.py
+++ tokencare/tokens/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from tokens.models import Token
+from tokens.serializers import NewTokenSerializer
 # Create your views here.
+
+def generate_token(patient_id):
+    # get current token number
+    current_token = Token.objects.order_by('-token_number').first()
+    if current_token is None:
+        next_token = 1
+    else:
+        next_token = current_token.token_number + 1
+        
+    # make entry in token table
+    data = {'token_number': next_token,
+            'patient':patient_id}
+    serializer = NewTokenSerializer(data=data)
+    if serializer.is_valid():
+        serializer.save()
+        print("__________________________")
+        print("Serializse.data inside generate token fix", serializer.data)
+        return next_token
+
+def list_tokens(request):
+    return HttpResponse("<h1>Welcome to tokens module of Tokencare</h1>")
