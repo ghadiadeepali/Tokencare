b2f89a7283ac841db3611445589a05abe4350ad1
ghadiadeepali <deepalinghadia1606@gmail.com>
2025-07-01 18:23:05 +0530

    added url pattern to include urls of tokens app

--- tokencare/tokencare/urls.py
+++ tokencare/tokencare/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/patients/', include('patients.urls'))
+    path('api/v1/patients/', include('patients.urls')),
+    path('api/v1/tokens/',include('tokens.urls'))
 ]
