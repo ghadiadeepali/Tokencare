1bc14be5fd9c7652447bc27d95e15919340ec603
ghadiadeepali <deepalinghadia1606@gmail.com>
2025-07-01 13:50:57 +0530

    created NewOTPSerializer, OTPSerializer and custom create() method in NewPatientSerializer to exclude otp field while making entry in Patients table

--- tokencare/patients/serializers.py
+++ tokencare/patients/serializers.py
@@ -1,14 +1,28 @@
 from rest_framework import serializers
-from patients.models import Patient
+from patients import models
 
 class PatientSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Patient
+        model = models.Patient
         fields = "__all__"
         
         
 class NewPatientSerializer(serializers.ModelSerializer):
+    otp = serializers.CharField(write_only=True)
     class Meta:
-        model = Patient
-        fields = ['name', 'phone_no', 'age']
+        model = models.Patient
+        fields = ['name', 'phone_no', 'age', "otp"]
+        
+    def create(self, validated_data):
+        validated_data.pop('otp', None)  # 🚨 Remove otp before passing to model
+        return models.Patient.objects.create(**validated_data)
     
+class NewOTPSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.OTP
+        fields = ["phone_no","otp"]
+        
+class OTPSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.OTP
+        fields = "__all__"
