abba2ddea2d5804b8db06a6c6654425e4ae40b77
ghadiadeepali <deepalinghadia1606@gmail.com>
2025-07-02 20:42:41 +0530

    expose Celery app via __init__.py for CLI usage

---
+++ tokencare/tokencare/__init__.py
@@ -0,0 +1,7 @@
+from .celery import app as celery_app
+
+__all__ = ('celery_app',)
+
+# this allows celery to be discovered when you run celery -A tokencare worker --loglevel=info
+# Celery knows that it has to load celery_app from your project’s __init__.py.
+# This makes your Celery app available when Django starts up — so that: celery -A tokencare worker can find and load your Celery app from tokencare/__init__.py.
