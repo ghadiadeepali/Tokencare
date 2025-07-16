9d71b8a0c887d0eedcae50e3f5dd9b20598405b2
ghadiadeepali <deepalinghadia1606@gmail.com>
2025-07-16 15:14:22 +0530

    celery task to send token message to patient

--- tokencare/patients/tasks.py
+++ tokencare/patients/tasks.py
@@ -1,11 +1,10 @@
 # write celery tasks here
 from celery import shared_task
 from time import sleep
+
 @shared_task
-def add_numbers(x, y):
-    print("++++++++++++++++++++++++++++++++++++++++++")
-    print("BEFORE SLEEPING")
-    sleep(10)
-    print("___________________________________________")
-    print("AFTER SLEEPING")
-    return x + y
+def send_token_message(phone_no, token_number):
+    message = f"Hello! Your hospital token number is {token_number}. Please wait for your turn. We’ll notify you when it’s close. Thank you!"
+    # Simulated SMS log
+    print(f"[DEV MODE] Simulated SMS to {phone_no}: {message}")
+    
