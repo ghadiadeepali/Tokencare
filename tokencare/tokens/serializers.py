9e75c7b50a7fea6db4d9c9403448a3401f1eb2bc
ghadiadeepali <deepalinghadia1606@gmail.com>
2025-07-19 11:59:26 +0530

    created TokenStatusUpdateSerializer

--- tokencare/tokens/serializers.py
+++ tokencare/tokens/serializers.py
@@ -5,3 +5,13 @@ class NewTokenSerializer(serializers.ModelSerializer):
     class Meta:
         model = Token
         fields = ["token_number","patient"]
+        
+class TokenSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Token
+        fields = "__all__"
+        
+class TokenStatusUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Token
+        fields = ["status"]
