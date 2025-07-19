79339fc7efe0d776a5203dd4cfd93e10c21cf62c
ghadiadeepali <deepalinghadia1606@gmail.com>
2025-07-19 11:59:49 +0530

    url pattern to update token status and list pending tokens

--- tokencare/tokens/urls.py
+++ tokencare/tokens/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views  # or your specific view file
 
 urlpatterns = [
-    path("", views.list_tokens)]
+    path("", views.list_tokens),
+    path("update/<int:token_number>/", views.update_token_status),
+    path("pending/", views.list_pending_tokens)]
